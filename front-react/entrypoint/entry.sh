1c28aa76b9c9d5d91a30e14fdb13ef987310e5aa
momo <ptjkjm1@gmail.com>
2024-04-26 10:14:21 +0900

    add environment (BACK_API_TIMEOUT) to front-end

--- front-react/entrypoint/entry.sh
+++ front-react/entrypoint/entry.sh
@@ -8,6 +8,7 @@ set -e
 echo "IS_INSIDE_IN_DOCKER_NOT_VALUE_FOR_USER=$IS_INSIDE_IN_DOCKER_NOT_VALUE_FOR_USER"; \
 echo "LANGUAGE=$LANGUAGE"; \
 echo "FRONT_BACK_HOST=$FRONT_BACK_HOST"; \
+echo "BACK_API_TIMEOUT=$BACK_API_TIMEOUT"; \
 } > /home/src/iae-setter/.env
 
 cd /home/src/iae-setter
